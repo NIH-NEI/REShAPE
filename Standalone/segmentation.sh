171533128cda417afba26683e17bf32f907795fa
Alex Mestiashvili <alex@biotec.tu-dresden.de>
2023-12-05 17:41:43 +0100

    Include system's LD_LIBRARY_PATH var, needed for --nv flag of singularity

--- Standalone/segmentation.sh
+++ Standalone/segmentation.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 exe_name=$0
 exe_dir=`dirname "$0"`
-export LD_LIBRARY_PATH=.:/opt/MatlabRT/v97/runtime/glnxa64:/opt/MatlabRT/v97/bin/glnxa64:/opt/MatlabRT/v97/sys/os/glnxa64:/opt/MatlabRT/v97/extern/bin/glnxa64
+export LD_LIBRARY_PATH=.:/opt/MatlabRT/v97/runtime/glnxa64:/opt/MatlabRT/v97/bin/glnxa64:/opt/MatlabRT/v97/sys/os/glnxa64:/opt/MatlabRT/v97/extern/bin/glnxa64:${LD_LIBRARY_PATH}
 args=
 while [ $# -gt 0 ]; do
     token=$1
